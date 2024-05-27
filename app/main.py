deffa4fcca0f95f6e339441b517a6de177a98dba
PranavTyagi-3 <pranavww@gmail.com>
2024-05-27 14:55:44 +0530

    Echo

--- app/main.py
+++ app/main.py
@@ -11,12 +11,13 @@ def main():
     data = client.recv(1024).decode()
     req = data.split('\r\n')
     if req[0].split(" ")[1] != "/":
-        response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+        if req[0].split('/')[1] == 'echo':
+            response ="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n"+req[0].split('/')[2].encode()
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
     else:
         response = "HTTP/1.1 200 OK\r\n\r\n".encode()
-    print(req[0].split('/'))
-    if req[0].split('/')[1] == 'echo':
-        response ="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n"+req[0].split('/')[2].encode()
+    
     client.send(response)
 
 if __name__ == "__main__":
