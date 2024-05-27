948ab087d42365b1c419a4e4794d357cb3221141
PranavTyagi-3 <pranavww@gmail.com>
2024-05-27 15:01:30 +0530

    Echo

--- app/main.py
+++ app/main.py
@@ -10,14 +10,13 @@ def main():
     client, addr = server_socket.accept()
     data = client.recv(1024).decode()
     req = data.split('\r\n')
-    if req[0].split(" ")[1] != "/":
-        if req[0].split('/')[1] == 'echo':
-            response ="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n"+req[0].split('/')[2].encode()
-        else:
-            response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+    path = req[0].split(" ")[1]
+    if path == "/":
+        response = "HTTP/1.1 200 OK\r\n\r\n".encode()  
+    elif path.startswith('/echo'):
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n{path[6:]}".encode()
     else:
-        response = "HTTP/1.1 200 OK\r\n\r\n".encode()
-    
+        response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
     client.send(response)
 
 if __name__ == "__main__":
