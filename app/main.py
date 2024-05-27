dfc19fa56fd2bedfafde137933a37242d9b37a68
PranavTyagi-3 <pranavww@gmail.com>
2024-05-27 14:40:31 +0530

    Check url

--- app/main.py
+++ app/main.py
@@ -7,8 +7,15 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-
+    client, addr = server_socket.accept()
+    data = client.recv(1024).decode()
+    req = data.split('\r\n')
+    print(req)
+    if req[0].split(" ")[1] != "/":
+        response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+    else:
+        response = "HTTP/1.1 200 OK\r\n\r\n".encode()
+    client.send(response)
 
 if __name__ == "__main__":
     main()
